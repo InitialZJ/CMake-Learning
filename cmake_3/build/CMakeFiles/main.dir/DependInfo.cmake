3fbad4e7c51a92e376f9edda5ec8cce84e2e3396
Jeffrey0418 <blueskyzzz0418@gmail.com>
2022-05-13 23:05:20 +0800

    新增cmake多文件编译

---
+++ cmake_3/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lzj/CMake-Learning/cmake_3/main.cpp" "/home/lzj/CMake-Learning/cmake_3/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/lzj/CMake-Learning/cmake_3/src/swapr.cpp" "/home/lzj/CMake-Learning/cmake_3/build/CMakeFiles/main.dir/src/swapr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_3/include/swapr.h
@@ -0,0 +1,4 @@
+#ifndef SWAPR_H_
+#define SWAPR_H_
+void swapr(int&a, int& b);
+#endif
