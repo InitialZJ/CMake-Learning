580f4c50bad59befb8d0e3052908fe75de5ccade
Jeffrey0418 <blueskyzzz0418@gmail.com>
2022-05-14 10:11:32 +0800

    新增cmake生成动态库

---
+++ cmake_5/build/bin/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/lzj/CMake-Learning/cmake_5/lib/hello.cpp" "/home/lzj/CMake-Learning/cmake_5/build/bin/CMakeFiles/hello.dir/hello.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake_5/lib/hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "hello.h"
+
+void helloFunc()
+{
+	std::cout << "Hello world!!!!!!!!" << std::endl;
+}
